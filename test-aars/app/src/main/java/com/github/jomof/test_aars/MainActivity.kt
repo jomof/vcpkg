638a3af0dd83381c2906fb32b59961421e1c26d1
Fisher Jomo <jomof@google.com>
2021-06-15 13:31:17 -0700

    Older AGP version

--- test-aars/app/src/main/java/com/github/jomof/test_aars/MainActivity.kt
+++ test-aars/app/src/main/java/com/github/jomof/test_aars/MainActivity.kt
@@ -7,28 +7,27 @@ import com.github.jomof.test_aars.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
     }
-
     /**
-     * A native method that is implemented by the 'test_aars' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+      * A native method that is implemented by the 'test_aars' native library,
+      * which is packaged with this application.
+      */
+     external fun stringFromJNI(): String
 
-    companion object {
-        // Used to load the 'test_aars' library on application startup.
-        init {
-            System.loadLibrary("test_aars")
-        }
-    }
+     companion object {
+         // Used to load the 'test_aars' library on application startup.
+         init {
+             System.loadLibrary("test_aars")
+         }
+     }
 }
